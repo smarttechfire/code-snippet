ce47f574eb75bd890b35f271292a00ec015b6ba2
smarttechfire <educationfire552@gmail.com>
2024-07-10 22:25:43 +0530

    add the call to action

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   return (
     <div className="">
       <Navbar/>
+      <CTASection />
     </div>
   );
 }
@@ -41,6 +42,23 @@ function Buttons(){
         Sign In
       </Button>
       <Button className={`text-sm border bg-white border-[#8338ec] text-[#8338ec] hover:bg-[#8338ec] hover:text-white p-[8px] px-6 rounded-md `}>Sign Up</Button>
+    </div>
+  );
+}
+
+function CTASection(){
+  return(
+    <div className=" flex flex-col mx-16 items-center mt-[120px] gap-6">
+      <h2 className=" font-bold text-2xl text-center">
+        Organize Your Code Snippets
+        <span className={`text-[#8338ec]`}>Efficiently!</span>
+      </h2>
+      <p className=" text-center text-sm w-[450px] max-sm:w-full text-slate-500">
+        With our advanced tagging and search features,you can quickly find the snippet you need, right when you need it. Spend less time searching for code and more time writing it.
+      </p>
+      <Button className={`block px-9 py-3 bg-white text-sm font-medium text-black hover:bg-white transition focus:outline-none`}>
+        {`Let's get Started!`}
+      </Button>
     </div>
   );
 }
